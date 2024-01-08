eba564fd62fc07fe2471aa12fe20e410fe28bbc0
TomBers <tjcberman@gmail.com>
2024-01-08 13:01:48 +0000

    added case studies as data

--- lib/assistant_engineering_web/controllers/page_controller.ex
+++ lib/assistant_engineering_web/controllers/page_controller.ex
@@ -4,7 +4,36 @@ defmodule AssistantEngineeringWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+
+    case_studies = [
+      %{
+        url: "https://chat.openai.com/g/g-JTGPg4Brc-credit-memo-assistant",
+        title: "Credit Memo",
+        description:
+          "Leveraged loan investors with precise document summaries and risk-aligned advice. Tooling: Prompt engineering, RAG",
+        image: "images/credit_memo.png"
+      },
+      %{
+        url: "https://chat.openai.com/g/g-XqAsoUyjd-gpts-prompt-evaluator",
+        title: "Prompt Evaluator",
+        description:
+          "Evaluates your GPTs instructions and suggests improvements. Tooling: Prompt engineering, RAG",
+        image: "images/prompt_evaluator.png"
+      },
+      %{
+        url: "https://chat.openai.com/g/g-HQRj6hJuR-notes-from-underground-tutor",
+        title: "Book Tutor",
+        description:
+          "Discuss themes and characters, save notes. Tooling: Prompt engineering, RAG, Actions (API)",
+        image: "images/notes_from_underground.png"
+      }
+    ]
+
+    render(conn, :home,
+      contact_email: "mailto:tjcberman@gmail.com",
+      case_studies: case_studies,
+      layout: false
+    )
   end
 
   def privacy(conn, _params) do
